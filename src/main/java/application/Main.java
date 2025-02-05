48fa6e3b94837d6c874c1a3691e36fd6a73d9bed
Vitor <hugocunhadiniz@gmail.com>
2025-02-04 21:01:01 -0300

    Finalizado

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,39 +2,85 @@ package application;
 
 import entities.Client;
 
+import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
     public static void main(String[] args) {
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Client client = new Client();
+        Client client;
 
 
         System.out.println("Enter Account number: ");
-        client.setAccount(sc.nextInt());
+        int number = sc.nextInt();
 
         System.out.println("Enter Account Holder: ");
-        client.setName(sc.nextLine()) ;
+        sc.nextLine();
+        String holder = sc.nextLine();
+
+        System.out.println("Is there na initial deposit (y/n)? ");
+        char response = sc.next().charAt(0);
+
+        if(response == 'y'){
+            System.out.println("Enter initial deposit value:" );
+            double initialDeposit = sc.nextDouble();
+            client = new Client(number,holder,initialDeposit);
+        }
+        else{
+            client = new Client(number,holder);
+        }
+
+        System.out.println("\nAccount data: ");
+        System.out.println(client);
+        System.out.print("\nEnter a deposit value:");
+        client.depositar(sc.nextDouble());
+
+        System.out.println("Update account data: ");
+        System.out.println(client);
+
+
+        System.out.print("\nEnter a witdraw value:");
+
+        client.sacar(sc.nextDouble());
+        System.out.println("Update account data:");
+        System.out.println(client);
+        sc.close();
+
+    /*    client.setAccount(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println("Enter Account Holder: ");
+        client.setName(sc.nextLine());
 
         System.out.println("Is there na initial deposit (y/n)? ");
         String confirm = sc.nextLine();
 
-      if (confirm == "y"){
+      if (confirm.equalsIgnoreCase("y")){
                 System.out.println("Enter initial deposit value: ");
-                double deposit = sc.nextDouble();
-                System.out.println("Account data: ");
-               
+                client.depositar(sc.nextDouble());
         }
+        System.out.println("\nAccount data: ");
+        System.out.printf("Account: %d, Holder: %s, Balance:$ %.2f \n", client.getAccount(),client.getName(),client.getBalance());
+
+        System.out.print("\nEnter a deposit value:");
+        client.depositar(sc.nextDouble());
+
+        System.out.println("Update account data: ");
+        System.out.printf("Account: %d, Holder: %s, Balance:$ %.2f \n", client.getAccount(),client.getName(),client.getBalance());
+
+
+        System.out.print("\nEnter a witdraw value:");
+        client.sacar(sc.nextDouble());
+        System.out.println("Update account data:");
+        System.out.printf("Account: %d, Holder: %s, Balance:$ %.2f \n", client.getAccount(),client.getName(),client.getBalance());
+        sc.close();*/
 
 
 
 
-
-
-
-        sc.close();
     }
 }
--- src/main/java/entities/Client.java
+++ src/main/java/entities/Client.java
@@ -15,19 +15,28 @@ public class Client {
         this.name = name;
     }
 
+    public Client(int account, String name, double initialDeposit){
+        this.account = account;
+        this.name = name;
+        depositar(initialDeposit);
+    }
+
     public int getAccount() {
         return account;
     }
 
     public void setAccount(int account) {
+
         this.account = account;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
@@ -35,17 +44,21 @@ public class Client {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
     public void depositar(double balance){
+
         this.balance += balance;
     }
 
     public void sacar(double balance){
-        this.balance -= balance;
+        this.balance -= balance + 5;
     }
 
-
+    public String toString(){
+        return "Account "
+                + account
+                + ", Holder:"
+                + name
+                +", Balance: $ "
+                + String.format("%.2f,",balance);
+    }
 }
